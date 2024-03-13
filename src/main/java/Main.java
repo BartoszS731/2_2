5b01e1e81ee76a9c8d652eb150f469c9395a7db8
BartoszS731 <161606110+BartoszS731@users.noreply.github.com>
2024-03-13 13:49:11 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,24 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+import java.util.Scanner;
+public class Main {
+  public static void main(String[] args) {
+
+Scanner scanner=new Scanner(System.in);
+    System.out.println("Prosze wprowadzic pierwsza liczbe:");
+    int a=scanner.nextInt();
+    System.out.println("Prosze wprowadzic drugą liczbe:");
+    int b=scanner.nextInt();
+    System.out.println("Prosze wprowadzic trzecią liczbe:");
+    int c=scanner.nextInt();
+    int Max=Math.max(a,Math.max(b,c));
+System.out.println("Najwieksza liczba to:"+Max);
+    scanner.close();
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
